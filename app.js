6f7db7dee8bf15972a076439edcefdec910dbb09
mrarya2002 <nikhilarya3108202@gmail.com>
2023-04-23 10:54:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+
+app.get("/download", async (req, res) => {
+    const url = req.query.link;
+
+    const v_id = ytdl.getURLVideoID(url);
+
+    const info = await ytdl.getInfo(url);
+    console.log(v_id)
+
+  return res.render("download", {
+    url: "https://www.youtube.com/embed/" + v_id,
+
+      info: info.formats.sort((a, b) => {
+          return a.mimeType < b.mimeType;
+      }),
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
