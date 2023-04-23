c6023adebf2cc940787fc1c9d35fb832a8193403
mrarya2002 <nikhilarya3108202@gmail.com>
2023-04-23 11:15:47 +0530

    index change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("main");
+  res.render("index");
 });
 
 app.get("/download", async (req, res) => {
